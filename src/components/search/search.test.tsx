3499a2c432b5428916fbf523df5e2c7f627a76f3
Alonso Cadenas <alonso.cadenas@gartner.com>
2021-06-06 22:36:57 -0500

    Enable users to browse categories.

---
+++ src/components/search/search.test.tsx
@@ -0,0 +1,5 @@
+import { Search } from './search';
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(Search(1, 2)).toBe(3);
+});
---
+++ src/components/search/search.tsx
@@ -0,0 +1,33 @@
+import styles from './search.module.css';
+import { useState } from 'react';
+import { sanitizeInput } from '../../utils/sanitizeInput';
+
+export function Search(a, b) {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [isSearching, setIsSearching] = useState(false);
+    const [results, setResults] = useState([]);
+
+    return (
+        <section className={styles.search}>
+            <header id={'search-header'}>
+                <input
+                    aria-label='search'
+                    autoComplete='off'
+                    id='search-input'
+                    onChange={(e) => {
+                        const value = sanitizeInput(e?.target?.value);
+                        if (value !== searchTerm) {
+                            setIsSearching(true);
+                            setSearchTerm(value);
+                        }
+                    }}
+                    onFocus={() => setShowDropdown(true)}
+                    placeholder='Search'
+                    type='text'
+                    value={searchTerm}
+                />
+            </header>
+        </section>
+    );
+}
