0a3fab8d1b2f1118cbba45a943257b4e22f47501
Alonso Cadenas <alonso.cadenas@gartner.com>
2021-06-23 06:40:24 -0500

    Update search component.

--- src/utils/getSearchResults/getSearchResults.ts
+++ src/utils/getSearchResults/getSearchResults.ts
@@ -1,8 +1,21 @@
+import { searchItems } from '../../graphql/queries';
+import { SearchItemsQuery } from '../../API';
+import { API } from 'aws-amplify';
+
 /**
  * Returns items that match the search query.
  * @param searchQuery
  */
-export function getSearchResults(searchQuery: string) {
-  // return Promise.resolve(['metal spoon', 'plastic spoon', 'wooden spoon']);
-  return Promise.resolve([]);
+export async function getSearchResults(searchQuery: string) {
+  let items = [];
+  if (searchQuery) {
+    const response = (await API.graphql({
+      query: searchItems,
+      variables: {
+        filter: { name: { wildcard: `*${searchQuery}*` } },
+      },
+    })) as { data: SearchItemsQuery };
+    items = response?.data?.searchItems?.items;
+  }
+  return items;
 }
