bb084ac702ba61b7c36a4418f884b550cb4617bb
Alonso Cadenas <alonso.cadenas@gartner.com>
2021-06-23 04:58:48 -0500

    Update home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,12 @@
-import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
-import { Amplify, API, Auth, withSSRContext } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import Head from 'next/head';
 import awsExports from '../aws-exports';
-import { createTodo } from '../graphql/mutations';
-import { listTodos } from '../graphql/queries';
-import {
-  CreateTodoInput,
-  CreateTodoMutation,
-  ListTodosQuery,
-  Todo,
-} from '../API';
-import { GRAPHQL_AUTH_MODE } from '@aws-amplify/api';
-import { useRouter } from 'next/router';
-import { GetServerSideProps } from 'next';
 import styles from '../styles/Home.module.css';
 import { Header, Search } from '../components';
 
 Amplify.configure({ ...awsExports, ssr: true });
 
-export default function HomePage({ todos = [] }: { todos: Todo[] }) {
-  const router = useRouter();
-
-  async function handleCreateTodo(event) {
-    event.preventDefault();
-
-    const form = new FormData(event.target);
-
-    try {
-      const createInput: CreateTodoInput = {
-        name: form.get('title').toString(),
-        description: form.get('content').toString(),
-      };
-
-      const request = (await API.graphql({
-        authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
-        query: createTodo,
-        variables: {
-          input: createInput,
-        },
-      })) as { data: CreateTodoMutation; errors: any[] };
-
-      router.push(`/todo/${request.data.createTodo.id}`);
-    } catch ({ errors }) {
-      console.error(...errors);
-      throw new Error(errors[0].message);
-    }
-  }
-
+export default function HomePage() {
   return (
     <section className={styles.container}>
       <Head>
@@ -61,64 +21,7 @@ export default function HomePage({ todos = [] }: { todos: Todo[] }) {
         <h2>Learn how to dispose of your household items</h2>
         <Search />
         <img id={'banner'} alt={'Banner'} src={'/banner.jpeg'} />
-
-        {/*<p className={styles.description}>*/}
-        {/*  <code className={styles.code}>{todos.length}</code>*/}
-        {/*  Todos*/}
-        {/*</p>*/}
-
-        {/*<div className={styles.grid}>*/}
-        {/*  {todos.map((todo) => (*/}
-        {/*    <a href={`/todo/${todo.id}`} key={todo.id}>*/}
-        {/*      <h3>{todo.name}</h3>*/}
-        {/*      <p>{todo.description}</p>*/}
-        {/*    </a>*/}
-        {/*  ))}*/}
-
-        {/*  <div className={styles.card}>*/}
-        {/*    <h3 className={styles.title}>New Appointment</h3>*/}
-
-        {/*    <AmplifyAuthenticator>*/}
-        {/*      <form onSubmit={handleCreateTodo}>*/}
-        {/*        <fieldset>*/}
-        {/*          <legend>Title</legend>*/}
-        {/*          <input*/}
-        {/*            defaultValue={`Today, ${new Date().toLocaleTimeString()}`}*/}
-        {/*            name="title"*/}
-        {/*          />*/}
-        {/*        </fieldset>*/}
-
-        {/*        <fieldset>*/}
-        {/*          <legend>Content</legend>*/}
-        {/*          <textarea*/}
-        {/*            defaultValue="I built an Amplify app with Next.js!"*/}
-        {/*            name="content"*/}
-        {/*          />*/}
-        {/*        </fieldset>*/}
-
-        {/*        <button>Create Todo</button>*/}
-        {/*        <button type="button" onClick={() => Auth.signOut()}>*/}
-        {/*          Sign out*/}
-        {/*        </button>*/}
-        {/*      </form>*/}
-        {/*    </AmplifyAuthenticator>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
       </main>
     </section>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const SSR = withSSRContext({ req });
-
-  const response = (await SSR.API.graphql({ query: listTodos })) as {
-    data: ListTodosQuery;
-  };
-
-  return {
-    props: {
-      todos: response.data.listTodos.items,
-    },
-  };
-};
