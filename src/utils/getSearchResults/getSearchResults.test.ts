b53ec99fc991fb095280bd6b0ee7ff225e059f5b
Alonso Cadenas <alonso.cadenas@gartner.com>
2021-06-23 07:05:18 -0500

    Update test snapshots.

--- src/utils/getSearchResults/getSearchResults.test.ts
+++ src/utils/getSearchResults/getSearchResults.test.ts
@@ -1,9 +1,22 @@
 import { getSearchResults } from './getSearchResults';
+import { API } from 'aws-amplify';
 
 describe('getSearchResults', () => {
   it('should do nothing if passed an empty string', async () => {
     const searchQuery = '';
     const expected = [];
+    const actual = await getSearchResults(searchQuery);
+    expect(actual).toEqual(expected);
+  });
+
+  it('should search for items', async () => {
+    const expected = [{ id: 123, name: 'test' }];
+    API.graphql = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        data: { searchItems: { items: expected } },
+      })
+    );
+    const searchQuery = 'ba';
     const actual = await getSearchResults(searchQuery);
     expect(actual).toEqual(expected);
   });
